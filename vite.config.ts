f32973d69afc1314dda5fba31745824dfb1f5db4
SHTHK <refplect020101@gmail.com>
2026-09-04 21:06:48 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/SC.github.io/',
+    base: '/SCH/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
